6a18d2f8eabf2bfb76747db2d1473af40ed33ce6
Anton <j10r2@mail.ru>
2022-08-05 20:53:55 +0300

    поиск max

--- project 5/Program.cs
+++ project 5/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Кто здесь? о_О");
+Console.WriteLine();
+string name = Console.ReadLine();
+
+if(name.ToLower() == "маша")
+{
+    Console.WriteLine();
+    Console.WriteLine("Шла бы ты отсюда, Маша...");
+}
+else
+{
+    Console.WriteLine();
+    Console.Write("Ну привет, ");
+    Console.Write(name);
+}
