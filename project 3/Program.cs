d0500be356ac0d08211586dff8871fb0c370f7a8
Anton <j10r2@mail.ru>
2022-08-05 20:31:02 +0300

    sum random

--- project 3/Program.cs
+++ project 3/Program.cs
@@ -1,5 +1,7 @@
-﻿int numA = 34;
-int numB = 100;
+﻿int numA = new Random().Next(1, 10);
+Console.WriteLine(numA);
+int numB = new Random().Next(1, 10);
+Console.WriteLine(numB);
 int result = numA + numB;
 Console.Write("сумма равна ");
 Console.Write(result);
